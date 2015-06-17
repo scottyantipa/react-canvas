1e209757e6ae1cc41b9e8f2c8f3afba53c17c08d
Scott Antipa <scottantipa@gmail.com>
2015-06-17 15:32:17 -0700

    Exporting react isntead of react-with-addons

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 // Index for the browserify process to create a dist
 // It attaches ReactCanvas and React to window for use by other libs
 
-window.React = require('react/dist/react-with-addons');
+window.React = require('react');
 
 var ReactCanvas = {
   Surface: require('../lib/Surface'),
